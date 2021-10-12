08d76e4635d889b15a134dd4c7f5e4f504535678
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-10-12 21:01:36 +0100

    denis vai trabalhar!

---
+++ 3DAndroidGame/Assets/Scripts/GamePlay/EndGameZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameZone : MonoBehaviour
+{
+   
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 1, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var destroyable = collision.gameObject.GetComponent<IDestroyable>();
+        if (destroyable != null)
+        {
+            destroyable.DestroyObj();
+
+            /*
+             * TODO: Add sound
+             *       Add anim
+             *       Remove Live
+             */
+        }
+    }
+}
