1cf051f98a4c5fc75e7aec6233ea0d6d7c6853c9
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-10-12 20:41:03 +0100

    destroyable obj done

---
+++ 3DAndroidGame/Assets/Scripts/Manager/ClickManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                var destroyable = hit.transform.gameObject.GetComponent<IDestroyable>();
+                if (destroyable != null) 
+                {
+                    destroyable.DestroyObj();
+
+                    /*
+                     * TODO: Add sound
+                     *       Add anim
+                     */       
+                }
+            }
+        }
+    }
+}
