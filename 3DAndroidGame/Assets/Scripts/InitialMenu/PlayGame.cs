98467f1231555c860cbe15fb16e93db5e496e8a0
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-10-12 19:26:16 +0100

    simple menu

---
+++ 3DAndroidGame/Assets/Scripts/InitialMenu/PlayGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fadeOut;
+    private float animDuration = 1f;
+
+    public void Play()
+    {
+        int indexToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+
+        StartCoroutine(LoadSceneByIndex(indexToLoad, animDuration));
+    }
+
+    private IEnumerator LoadSceneByIndex(int i, float time)
+    {
+        fadeOut.SetActive(true);
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(i);
+    }
+}
