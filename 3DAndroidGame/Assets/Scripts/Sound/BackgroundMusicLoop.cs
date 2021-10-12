9dba4ce7c81e555cca9ba7774a267dba106c27f6
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-10-12 19:50:51 +0100

    background sound done, need music

---
+++ 3DAndroidGame/Assets/Scripts/Sound/BackgroundMusicLoop.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusicLoop : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+    private int currentClip = 0;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        PlayClip();
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            PlayClip();
+        }
+    }
+
+    private AudioClip GetNextClip()
+    {
+        AudioClip clip = clips[currentClip];
+
+        currentClip++;
+        if (currentClip >= clips.Length)
+        {
+            currentClip = 0;
+        }
+
+        return clip;
+    }
+
+    private void PlayClip()
+    {
+        if(clips.Length > 0)
+        {
+            audioSource.clip = GetNextClip();
+            audioSource.Play();
+        }
+    }
+}
