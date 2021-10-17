6f70cb38b669e3a03e244c475169d98acf160a44
Denis <dennydenii@gmail.com>
2021-10-17 02:35:17 +0100

    ficaram cenas para tras wtf

---
+++ 3DAndroidGame/Assets/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    public int clicksToDestroy;
+
+    [SerializeField]
+    private int velocity;
+
+    [SerializeField]
+    private int upDownValue;
+
+
+    public int clicksCount { get; set; }
+
+    #region getters
+        public int GetUpDownValue() { return upDownValue; }
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(0, -velocity, 0) * Time.deltaTime;
+    }
+
+    public void IncClicks()
+    {
+        clicksCount++;
+    }
+}
