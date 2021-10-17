e0ee7e61a82877cf86cff92f9863e59572b76b73
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-10-17 19:40:40 +0100

    sound

--- 3DAndroidGame/Assets/Scripts/GamePlay/SelfDestroy.cs
+++ 3DAndroidGame/Assets/Scripts/GamePlay/SelfDestroy.cs
@@ -3,38 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-[RequireComponent(typeof(AudioSource))]
 public class SelfDestroy : MonoBehaviour, IDestroyable
 {
     public static Action OnAesteroidDestroyed;
 
-    [SerializeField]
-    private AudioClip destroyedSound;
-
-    private AudioSource audioSource;
-
-    private void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
+  
 
     public void DestroyObj(bool destroyedByPlayer)
     {
         // Only destroys the obj one time
         if (destroyedByPlayer)
         {
-            //PlaySound();
             //TODO: PLAY ANIM 
             FindObjectOfType<BotEndGameZone>().MoveEndZone(gameObject, true);
             OnAesteroidDestroyed?.Invoke();
 
         }
         Destroy(gameObject);
-    }
-
-    private void PlaySound()
-    {
-        audioSource.clip = destroyedSound;
-        audioSource.Play();
     }
 }
