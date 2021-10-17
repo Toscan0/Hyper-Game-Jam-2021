9d2510321031e8685136c447eaec39626e295d5b
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-10-17 19:07:59 +0100

    restart

---
+++ 3DAndroidGame/Assets/Scripts/UI/InitialMenu/RestartGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fadeOut;
+
+    [SerializeField]
+    private PointsManager pointsManager;
+
+    private float animDuration = 1f;
+
+    public void Restart()
+    {
+
+        pointsManager.Restart();
+
+        int indexToLoad = SceneManager.GetActiveScene().buildIndex - 1;
+        Debug.Log(indexToLoad);
+        StartCoroutine(LoadSceneByIndex(indexToLoad, animDuration));
+    }
+
+    private IEnumerator LoadSceneByIndex(int i, float time)
+    {
+        fadeOut.SetActive(true);
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(i);
+    }
+}
